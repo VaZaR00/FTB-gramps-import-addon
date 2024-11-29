37bfb59bd1f4cd4dfeb9d3b6ecb870eb6d293bfe
Vazar00 <aleksandr.mincenko01@gmail.com>
2024-11-29 17:15:16 +0200

    name change

--- ftb_gramps_sync.gpr.py
+++ ftb_gramps_sync.gpr.py
@@ -1,7 +1,7 @@
 #
 # Gramps - a GTK+/GNOME based genealogy program
 #
-# Copyright (C) 2024-2025 Vazar
+# Copyright (C) 2024-2025 Vazar00
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
